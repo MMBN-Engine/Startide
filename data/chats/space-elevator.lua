832c959d1eed4c8132674026ab51f3df11717d06
bdota <bendolata@yahoo.com>
2013-08-11 22:02:58 -0400

    fix

--- data/chats/space-elevator.lua
+++ data/chats/space-elevator.lua
@@ -2,7 +2,7 @@ newChat{ id="welcome",
 	text = [[#LIGHT_GREEN#Choose destination.]],
 	answers = {
 		{"My shuttle",
-			action = function(npc, player) game:registerDialog(require("mod.dialogs.ShuttleControl").new(player)) end
+			action = function(npc, player) game:changeLevel(1, "hanger") end
 		},
 		{"The surface",
 			action = function(npc, player) game:changeLevel(1, game.zone.elevator) end

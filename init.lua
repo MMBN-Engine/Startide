5b800a28e8929402f8fb598b76a9b36bd2529662
bdota <bendolata@yahoo.com>
2013-08-16 22:38:46 -0400

    spellcheck!

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@ homepage = "http://te4.org/"
 version = {0,0,1}
 engine = {1,0,1,"te4"}
 description = [[
-It is a unprecidented time of growth for the Children of Earth. Mankind has uplifted many species of Hominidae and Cetacea to full sophononce, with ongiong projects for the uplift of Corvidae and Octopoda. Machine intelligences are now comonplace. Humans have engineered themselves for many different habitats.The Solar System is teeming with life; Mars has been partialy teraformed, there are floating cities in the atmosphere of Venus, and numerous socities make their home in the Asteroid Belt and Outer System. Dozens of ships have been sent to the stars.
+It is a unprecedented time of growth for the Children of Earth. Mankind has uplifted many species of Hominidae and Cetacea to full sophononce, with ongiong projects for the uplift of Corvidae and Octopoda. Machine intelligences are now comonplace. Humans have engineered themselves for many different habitats. The Solar System is teeming with life; Mars has been partially terraformed, there are floating cities in the atmosphere of Venus, and numerous societies make their home in the Asteroid Belt and Outer System. Dozens of ships have been sent to the stars.
 
 But all is not well. Luddite factions on Mars oppress the local machine intelligences. Uplifts are discriminated against in nearly every society. The Outer System is in a constant state of war. Meanwhile, the Technarch of Earth sees all this and contemplates war to bring order to chaos.
 ]]

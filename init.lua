32251b26cff0f6fceadcc71b851f8353bd737feb
bdota <bendolata@yahoo.com>
2013-08-16 22:11:08 -0400

    first release!

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ long_name = "Startide"
 short_name = "startide"
 author = { "BDota" }
 homepage = "http://te4.org/"
-version = {1,0,0}
+version = {0,0,1}
 engine = {1,0,1,"te4"}
 description = [[
 It is a unprecidented time of growth for the Children of Earth. Mankind has uplifted many species of Hominidae and Cetacea to full sophononce, with ongiong projects for the uplift of Corvidae and Octopoda. Machine intelligences are now comonplace. Humans have engineered themselves for many different habitats.The Solar System is teeming with life; Mars has been partialy teraformed, there are floating cities in the atmosphere of Venus, and numerous socities make their home in the Asteroid Belt and Outer System. Dozens of ships have been sent to the stars.

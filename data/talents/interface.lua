94051e09ef79fabc92e5012692a943d4ff6363c5
bdota <bendolata@yahoo.com>
2013-08-30 19:56:04 -0400

    more combat interface talents

--- data/talents/interface.lua
+++ data/talents/interface.lua
@@ -1,8 +1,8 @@
-newTalentType{ type="interface", name = "interface", description = "Interface with machines" }
+newTalentType{ type="combat interface", name = "interface", description = "Interface with weapons" }
 
 newTalent{
-	name = "Machine Interface",
-	type = {"interface", 1},
+	name = "Combat Interface",
+	type = {"combat interface", 1},
 	points = 1,
 	mode = "passive",
 	on_learn = function(self, t)
@@ -14,6 +14,38 @@ newTalent{
 		self:addSlot("COMBAT_SOFTWARE", -2)
 	end,
 	info = function(self, t)
-		return [[Increases intelligence by 1 and allow interface with machines.]]
+		return [[Increases intelligence by 1 and gives two combat program slots.]]
+	end,
+}
+
+newTalent{
+	name = "Improved Combat Interface",
+	type = {"combat interface", 2},
+	points = 1,
+	mode = "passive",
+	on_learn = function(self, t)
+		self:addSlot("COMBAT_SOFTWARE", 1)
+	end,
+	on_unlearn = function(self, t)
+		self:addSlot("COMBAT_SOFTWARE", -1)
+	end,
+	info = function(self, t)
+		return [[You improve your combat algorithms, allow the use of an aditional combat program.]]
+	end,
+}
+
+newTalent{
+	name = "Advanced Combat Interface",
+	type = {"combat interface", 3},
+	points = 1,
+	mode = "passive",
+	on_learn = function(self, t)
+		self:addSlot("COMBAT_SOFTWARE", 1)
+	end,
+	on_unlearn = function(self, t)
+		self:addSlot("COMBAT_SOFTWARE", -1)
+	end,
+	info = function(self, t)
+		return [[You improve your combat algorithms, allow the use of an aditional combat program.]]
 	end,
 }

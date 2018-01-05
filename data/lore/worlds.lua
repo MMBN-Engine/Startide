06fa55f4a9f280a998f06934816ac49445d4cbae
bdota <bendolata@yahoo.com>
2018-01-04 17:56:38 -0800

    added lore entries for earth and mars

---
+++ data/lore/worlds.lua
@@ -0,0 +1,17 @@
+newLore{
+	id = "worlds-earth",
+	category = "worlds",
+	name = "Earth",
+	lore = [[Earth is a utopia: disease has been eradicated, violent crime is practically non-existent, and even the poorest life a life of luxury unimaginable to the rest of the system.  The working class consists primarily of baseline humans and uplifts, primarily chimpanzees, gorillas, and corvids. Vocational genetic enhancement is rare, with most preferring non-permanent implants. The species identity of the wealthiest inhabitants is typically indeterminate, as the well-to-do modify their bodies according to fashion trends or personal preferences.
+	
+	Each nation of Earth is a participatory democracy, although the day to day affairs of government are typically carried out by AGIs. The freedom of movement of peoples, goods, and capital between nations is absolute. Areas of competence of an international character, such as regulation and standardization of production, intersystem trade, immigration, and defense are legislated by supranational organizations similar to the old continental trade blocks of the 22nd century.]],
+}
+
+newLore{
+	id = "worlds-mars",
+	category = "worlds",
+	name = "Mars",
+	lore = [[Mars is a Blue Planet; where once there was barren desert, now there is life. The closest analogy to the current Martian socio-economic landscape is found in 21st century Earth. Mars is divided into dozens of nation states, each of which has internal sovereignty and well-defined borders with their neighbors. The economy is globalized, and trade is a significant source of pride and prestige.
+
+	AGIs are heavily restricted on Mars. In most nations, labor is carried out by a genetically modified underclass, while the managerial and ruling classes tend to be composed primarily of baseline humans. Uplift are often discriminated against on Mars. Historically, this discrimination has taken the form of prejudicial practices in employment and social biases among the human majority, although legalized persecution is becoming increasingly widespread.]],
+}
